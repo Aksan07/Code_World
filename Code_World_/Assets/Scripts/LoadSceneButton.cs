ea523ca2662540aac88ebf498d97958f622da2f8
Aksan Pramod <116940380+Aksan07@users.noreply.github.com>
2024-02-11 14:50:39 +0530

    All Scenes Imported , main map camera drag done

---
+++ Code_World_/Assets/Scripts/LoadSceneButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    [SerializeField]private String sceneName;
+   // Update is called once per frame
+    void Update()
+    {
+        OnMouseOver();
+    }
+    void OnMouseOver(){
+        if (Input.GetMouseButtonUp(0)){
+            Debug.Log("Test");
+            GoToScene(sceneName);
+        }
+    }
+    public void GoToScene(string sceneName){
+       StartCoroutine(LoadAsynchronously(sceneName));
+    }
+    IEnumerator LoadAsynchronously(string sceneName){
+         AsyncOperation operations =SceneManager.LoadSceneAsync(sceneName);
+            yield return null;
+         }
+}
